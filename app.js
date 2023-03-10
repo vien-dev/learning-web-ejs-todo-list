93ff81c88590508a2c1de00565eefc5711f6bf7f
Vien Mai <56462929+vien-dev@users.noreply.github.com>
2023-03-10 12:51:06 +0100

    First functional version
    NOTE: the version is functional BUT vulnerable to HMTL injection.
    This is to due the fact that:
    - We don't check user input
    - We use unescaped HTML tag <%- from EJS
    
    To be fixed in next version.

--- app.js
+++ app.js
@@ -1,15 +1,41 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const dayjs = require('dayjs');
+const weekOfYear = require('dayjs/plugin/weekOfYear')
+
+dayjs.extend(weekOfYear);
 
 const app = express();
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: false }))
+
+var toDoItems = [];
+
+function getTodayStr() {
+    var today = new Date();
+    var todayOptions = {weekday: "long", day: "numeric", month: "long", year: "numeric"};
+    var currentWeekNumber = dayjs().week();
+
+    return `Week ${currentWeekNumber}: ${today.toLocaleString("default", todayOptions)}`;
+}
+
+function renderResponseForMainRoute(res) {
+    toDoItemsHtmlStr = "";
+    toDoItems.forEach((toDoItem, idx) => toDoItemsHtmlStr += `<li>${toDoItem}</li>`);
+
+    res.render("list", {listEJSDay: getTodayStr(), listEJSToDoItems: toDoItemsHtmlStr});
+}
 
 app.get("/", (req, res) => {
-    day = new Date().toLocaleString("default", {weekday: "long"});
-    
-    res.render("list", {nameOfDay: day});
+    renderResponseForMainRoute(res);
 })
+
+app.post("/", (req, res) => {
+    toDoItems.push(req.body.toDoItem);
+
+    renderResponseForMainRoute(res);
+});
 
 app.listen(3000, () => {
     console.log("ToDo List server starts to serve at port 3000.");
